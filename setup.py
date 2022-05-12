d8a45d259a6de7a9a3ab8a2587f0d8c51d21dca6
Yousef Mohamad Abdala <95507626+blackaries506@users.noreply.github.com>
2022-05-12 12:18:33 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import os
+os.system("python3 -m pip install -r requirements.txt")
+
+setup(
+    name='setup',
+    version='1.0',
+    packages=['en'],
+    url='',
+    license='',
+    author='yousef mohamad abdala',
+    author_email='yousefmohamad506@gmail.com',
+    description=''
+)
